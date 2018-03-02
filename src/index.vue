392d245171359ceff19fb0cd43dd9649d9eb6c71
Administrator@XTM-01701112049 <502583304@qq.com>
2018-03-02 22:34:25 +0800

    搭建App组件页面布局

--- src/index.vue
+++ src/index.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="wrapper">
-    <image :src="logo" class="logo" />
-    <text class="greeting">The environment is ready!</text>
     <m-header></m-header>
+    <tab-bar></tab-bar>
     <router-view/>
   </div>
 </template>
 
 <script>
 import MHeader from '@/components/header/header.vue'
-
+import TabBar from '@/components/tab-bar/tab-bar.vue'
 export default {
   components: {
-    MHeader
+    MHeader,
+    TabBar
   },
   name: 'App',
   data () {
@@ -25,22 +25,10 @@ export default {
 
 <style scoped>
   .wrapper {
-    justify-content: center;
-    align-items: center;
-  }
-  .logo {
-    width: 424px;
-    height: 200px;
-  }
-  .greeting {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
     text-align: center;
-    margin-top: 70px;
-    font-size: 50px;
-    color: #41B883;
-  }
-  .message {
-    margin: 30px;
-    font-size: 32px;
-    color: #727272;
+    color: #2c3e50;
   }
 </style>
