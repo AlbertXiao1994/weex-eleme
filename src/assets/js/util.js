48182b752d18cdf3129d99d69ea8b0fe6a195a2f
Administrator@XTM-01701112049 <502583304@qq.com>
2018-03-03 14:56:54 +0800

    导出函数

--- src/assets/js/util.js
+++ src/assets/js/util.js
@@ -1,12 +1,10 @@
 /* eslint-disable no-undef */
 const stream = weex.requireModule('stream')
 
-export default {
-  get (url, callback) {
-    return stream.fetch({
-      methods: 'GET',
-      type: 'json',
-      url: url
-    }, callback)
-  }
+export function get (url, callback) {
+  return stream.fetch({
+    methods: 'GET',
+    type: 'json',
+    url: url
+  }, callback)
 }
