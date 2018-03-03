eb6aab6782e5da7c108ccb99068b7418329562aa
Administrator@XTM-01701112049 <502583304@qq.com>
2018-03-03 23:39:59 +0800

    增加初始化图标字体函数

--- src/assets/js/util.js
+++ src/assets/js/util.js
@@ -16,3 +16,16 @@ export function get (url, callback) {
     url: url
   }, callback)
 }
+
+/**
+ * @func
+ * @desc 初始化iconfont
+ * @export
+ */
+export function initIconFont () {
+  let domModule = weex.requireModule('dom')
+  domModule.addRule('fontFace', {
+    'fontFamily': 'iconfont',
+    'src': "url('http://at.alicdn.com/t/font_404010_f29c7wlkludz33di.ttf')"
+  })
+}
