57cb5a45448a90399e225559ea7d9b3413fdba3e
Administrator@XTM-01701112049 <502583304@qq.com>
2018-03-03 16:22:17 +0800

    增加函数注释

--- src/assets/js/util.js
+++ src/assets/js/util.js
@@ -1,6 +1,14 @@
 /* eslint-disable no-undef */
 const stream = weex.requireModule('stream')
 
+/**
+ * @func
+ * @desc 使用stream模块获取数据
+ * @export
+ * @param {string} url - 接口url
+ * @param {any} callback - 回调函数
+ * @returns
+ */
 export function get (url, callback) {
   return stream.fetch({
     methods: 'GET',
